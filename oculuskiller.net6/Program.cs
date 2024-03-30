4acb87a756efeab318447c232dc7e068b82ddf63
Bink-Lab <scoptixy@gmail.com>
2024-03-30 23:09:01 +0100

    Add project files.

---
+++ oculuskiller.net6/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Diagnostics;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string[] processNames = { "OculusClient", "OVRRedir", "OVRServer_x64", "OVRServiceLauncher" };
+
+        foreach (string processName in processNames)
+        {
+            Process[] processes = Process.GetProcessesByName(processName);
+
+            if (processes.Length > 0)
+            {
+                foreach (Process process in processes)
+                {
+                    Console.WriteLine($"Terminating process: {process.ProcessName} (ID: {process.Id})");
+                    process.Kill();
+                }
+            }
+            else
+            {
+                Console.WriteLine($"No process with the name '{processName}' found.");
+            }
+        }
+
+        Console.WriteLine("Press any key to exit...");
+        Console.ReadLine();
+    }
+}
